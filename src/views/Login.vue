948cb73d355d7cb746af24be63f0fad45f1908e6
vinnyk <ragdolleye@gmail.com>
2021-06-28 13:03:13 -0500

    Finishes login.vue functionality, updates main.js with import axios, updates main.js with jwt functionality.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="login">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form v-on:submit.prevent="submit()">
+      <h1>Login</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Email:</label>
+        <input type="email" v-model="email" />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" v-model="password" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Login",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      email: "",
+      password: "",
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function () {
+      var params = {
+        email: this.email,
+        password: this.password,
+      };
+      axios
+        .post("/sessions", params)
+        .then((response) => {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          this.$router.push("/about"); //This needs to change to push to student resume/capstone data
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        });
+    },
   },
 };
 </script>

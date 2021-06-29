1377e7a6fd6fa42bc650365c8f90398128e444eb
vinnyk <ragdolleye@gmail.com>
2021-06-29 15:08:48 -0500

    Adds deleteCapstone() and adds catch to editCapstone().

--- src/views/CapstonesEdit.vue
+++ src/views/CapstonesEdit.vue
@@ -1,43 +1,45 @@
 <template>
-  <div class="capstone">
-    <form v-on:submit.prevent="updateCapstone()">
+  <div class="capstones">
+    <form v-on:submit.prevent="editCapstone()">
       <h1>Edit Capstone</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </li>
-      </ul>
-
       <div class="form-group">
         <label>Name:</label>
-        <input type="text" class="form-control" v-model="capstone.name" />
+        <input
+          type="text"
+          class="form-control"
+          v-model="currentCapstone.name"
+        />
       </div>
       <div class="form-group">
         <label>Description:</label>
         <input
           type="text"
           class="form-control"
-          v-model="capstone.description"
+          v-model="currentCapstone.description"
           placeholder="name"
         />
       </div>
       <div class="form-group">
         <label>Url:</label>
-        <input type="text" class="form-control" v-model="capstone.url" />
+        <input type="text" class="form-control" v-model="currentCapstone.url" />
       </div>
       <div class="form-group">
         <label>Image:</label>
         <input
           type="text"
           class="form-control"
-          v-model="capstone.screenshot"
-          placeholder="Capstone Screenshot Image"
+          v-model="currentCapstone.screenshot"
         />
       </div>
 
       <input type="submit" class="btn btn-primary" value="Submit" />
     </form>
-    <p>{{ capstone }}</p>
+    <ul>
+      <li class="text-danger" v-for="error in errors" v-bind:key="error">
+        {{ error }}
+      </li>
+    </ul>
+    <button v-on:click="deleteCaptstone()">Delete Capstone</button>
   </div>
 </template>
 
@@ -46,27 +48,44 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      capstone: {
-        name: "",
-        description: "",
-        url: "",
-        screenshot: "",
+      errors: [],
+      currentCapstone: {
+        name: "clone a matic",
+        description: "clones the world on fire",
+        url: "http://www.somecloneapp.com",
+        screenshot: "http://somescreenshot.jpg",
       },
     };
   },
   created: function () {
-    axios.get(`"/capstone/${this.$route.params.id}"`).then((response) => {
+    axios.get(`/capstones/${this.$route.params.id}`).then((response) => {
       console.log("Capstone Object: ", response.data);
-      this.capstone = response.data;
+      this.editCapstone = response.data;
     });
   },
   methods: {
-    updateCapstone: function () {
-      axios.patch(`"/capstone/${this.$route.params.id}"`).then((response) => {
-        console.log("Updated Capstone Object:", response.data);
-        this.capstone = response.data;
-      });
+    editCapstone: function () {
+      axios
+        .patch(`/capstones/${this.$route.params.id}`)
+        .then((response) => {
+          console.log("Updated Capstone Object:", response.data);
+          this.capstone = response.data;
+          this.$router.push("/profile");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+          console.log(error.response.data.message);
+        });
+    },
+    deleteCaptstone: function () {
+      if (confirm("Do you really want to delete this capstone?")) {
+        axios
+          .delete(`/capstones/${this.currentCapstone.id}`)
+          .then((response) => {
+            console.log(response.data);
+            this.$router.push("/profile");
+          });
+      }
     },
   },
 };

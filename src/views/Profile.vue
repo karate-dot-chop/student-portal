4abc635a07fe10d3d1a0dc56c5088b7b43409e30
vinnyk <ragdolleye@gmail.com>
2021-06-30 13:38:40 -0500

    fixes moved files

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,51 +2,18 @@
   <div class="about">
     <div>
       <h1>Student Profile</h1>
-      <p>
-        <b>First Name:</b>
-        {{ currentStudent.first_name }}
-      </p>
-      <p>
-        <b>Last Name:</b>
-        {{ currentStudent.last_name }}
-      </p>
-      <p>
-        <b>Email:</b>
-        {{ currentStudent.email }}
-      </p>
-      <p>
-        <b>Phone:</b>
-        {{ currentStudent.phone_number }}
-      </p>
-      <p>
-        <b>Bio:</b>
-        {{ currentStudent.bio }}
-      </p>
-      <p>
-        <b>LinkedIn:</b>
-        {{ currentStudent.linkedin_url }}
-      </p>
-      <p>
-        <b>Twitter:</b>
-        {{ currentStudent.twitter_handle }}
-      </p>
-      <p>
-        <b>Personal Site:</b>
-        {{ currentStudent.personal_url }}
-      </p>
-      <p>
-        <b>Resume:</b>
-        {{ currentStudent.resume_url }}
-      </p>
-      <p>
-        <b>Github:</b>
-        {{ currentStudent.github_url }}
-      </p>
-      <p>
-        <b>Photo Url:</b>
-        {{ currentStudent.photo_url }}
-      </p>
-      <router-link to="/students/1/edit">Edit Student Info</router-link>
+      <p><b>First Name: </b>{{ currentStudent.first_name }}</p>
+      <p><b>Last Name: </b>{{ currentStudent.last_name }}</p>
+      <p><b>Email: </b>{{ currentStudent.email }}</p>
+      <p><b>Phone: </b>{{ currentStudent.phone_number }}</p>
+      <p><b>Bio: </b>{{ currentStudent.bio }}</p>
+      <p><b>LinkedIn: </b>{{ currentStudent.linkedin_url }}</p>
+      <p><b>Twitter: </b>{{ currentStudent.twitter_handle }}</p>
+      <p><b>Personal Site: </b>{{ currentStudent.personal_url }}</p>
+      <p><b>Resume: </b>{{ currentStudent.resume_url }}</p>
+      <p><b>Github: </b>{{ currentStudent.github_url }}</p>
+      <p><b>Photo Url: </b>{{ currentStudent.photo_url }}</p>
+      <router-link :to="`/students/1/edit`">Edit Student Info</router-link>
       <!--Needs soft-coded :id later-->
     </div>
     <div>
@@ -54,27 +21,12 @@
       <router-link to="/educations/new">New Education</router-link>
       <br />
       <div v-for="education in educations" v-bind:key="education.id">
-        <p>
-          <b>University:</b>
-          {{ education.university }}
-        </p>
-        <p>
-          <b>Degree:</b>
-          {{ education.degree }}
-        </p>
-        <p>
-          <b>Start Date:</b>
-          {{ education.start_date }}
-        </p>
-        <p>
-          <b>End_Date:</b>
-          {{ education.end_date }}
-        </p>
-        <p>
-          <b>Details:</b>
-          {{ education.details }}
-        </p>
-        <router-link to="/educations/1/edit">Edit Education</router-link>
+        <p><b>University:</b> {{ education.university }}</p>
+        <p><b>Degree:</b> {{ education.degree }}</p>
+        <p><b>Start Date:</b> {{ education.start_date }}</p>
+        <p><b>End_Date:</b> {{ education.end_date }}</p>
+        <p><b>Details:</b> {{ education.details }}</p>
+        <router-link :to="`/educations/1/edit`">Edit Education</router-link>
         <!--Needs soft-coded :id later-->
       </div>
     </div>
@@ -83,27 +35,12 @@
       <router-link to="/experiences/new">New Experience</router-link>
       <br />
       <div v-for="experience in experiences" v-bind:key="experience.id">
-        <p>
-          <b>Company:</b>
-          {{ experience.company_name }}
-        </p>
-        <p>
-          <b>Job Title:</b>
-          {{ experience.job_title }}
-        </p>
-        <p>
-          <b>Start Date:</b>
-          {{ experience.start_date }}
-        </p>
-        <p>
-          <b>End Date:</b>
-          {{ experience.end_date }}
-        </p>
-        <p>
-          <b>Details:</b>
-          {{ experience.details }}
-        </p>
-        <router-link to="/experiences/1/edit">Edit Experience</router-link>
+        <p><b>Company:</b> {{ experience.company_name }}</p>
+        <p><b>Job Title:</b> {{ experience.job_title }}</p>
+        <p><b>Start Date:</b> {{ experience.start_date }}</p>
+        <p><b>End Date:</b> {{ experience.end_date }}</p>
+        <p><b>Details:</b> {{ experience.details }}</p>
+        <router-link :to="`/experiences/1/edit`">Edit Experience</router-link>
         <!--Needs soft-coded :id later-->
       </div>
     </div>
@@ -111,48 +48,30 @@
       <h1>Skills</h1>
       <div v-for="skill in skills" v-bind:key="skill.id">
         <p>
-          {{ skill.name }}
-          <button v-on:click="destroySkill">Delete</button>
+          {{ skill.name }} <button v-on:click="destroySkill()">Delete</button>
         </p>
       </div>
       <div>
         <input v-model="newSkill" type="text" placeholder="New Skill" />
         <button v-on:click="createSkill()">Add Skill</button>
       </div>
+      <p>{{ newSkill }}</p>
     </div>
     <div>
       <h1>Capstones</h1>
       <router-link to="/capstones/new">New Capstone</router-link>
       <br />
       <div v-for="capstone in capstones" v-bind:key="capstone.id">
-        <p>
-          <b>Name:</b>
-          {{ capstone.name }}
-        </p>
-        <p>
-          <b>Description:</b>
-          {{ capstone.description }}
-        </p>
-        <p>
-          <b>URL:</b>
-          {{ capstone.url }}
-        </p>
-        <p>
-          <b>Image:</b>
-          {{ capstone.image }}
-        </p>
-        <router-link to="/capstones/1/edit">Edit Capstone</router-link>
+        <p><b>Name:</b> {{ capstone.name }}</p>
+        <p><b>Description:</b> {{ capstone.description }}</p>
+        <p><b>URL:</b> {{ capstone.url }}</p>
+        <p><b>Image:</b> {{ capstone.image }}</p>
+        <router-link :to="`/capstones/1/edit`">Edit Capstone</router-link>
         <!--Needs soft-coded :id later-->
       </div>
     </div>
   </div>
 </template>
-
-<style>
-p b {
-  font-weight: bold;
-}
-</style>
 
 <script>
 import axios from "axios";
@@ -164,13 +83,12 @@ export default {
       educations: [],
       experiences: [],
       skills: [],
-      newSkill: "",
+      newSkill: "dfdf",
       capstones: [],
     };
   },
   created: function () {
-    axios.get("/students/1").then((response) => {
-      //needs soft-coded id for students
+    axios.get(`/students/1`).then((response) => {
       console.log("Students Object: ", response.data);
       this.currentStudent = response.data;
     });
@@ -192,14 +110,6 @@ export default {
     });
   },
   methods: {
-    destroySkill: function () {
-      if (confirm("Are you sure you want to delete this skill?")) {
-        axios.delete(`/skills/${this.skill.id}`).then((response) => {
-          console.log(response.data);
-          this.$router.push("/skills");
-        });
-      }
-    },
     createSkill: function () {
       axios
         .post("/skills", this.newSkill)
@@ -210,6 +120,14 @@ export default {
         .catch((error) => {
           console.log(error.response.data.errors);
         });
+    },
+    destroySkill: function () {
+      if (confirm("Are you sure you want to delete this skill?")) {
+        axios.delete(`/skills/${this.skill.id}`).then((response) => {
+          console.log(response.data);
+          this.$router.push("/skills");
+        });
+      }
     },
   },
 };
